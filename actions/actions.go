58b9f380eb8781485cec299587caf2420ef5f758
Ryan Weald <ryan@weald.com>
2014-12-18 00:12:59 -0800

    Prepare to have handler that actually reads text input

--- actions/actions.go
+++ actions/actions.go
@@ -1,5 +1,7 @@
 package actions
 
+import "errors"
+
 type SlackAction interface {
 	HandleAction(m *SlackMessage) (error, *SlackResponse)
 }
@@ -11,5 +13,18 @@ func init() {
 type HelloAction struct{}
 
 func (h HelloAction) HandleAction(m *SlackMessage) (error, *SlackResponse) {
-	return nil, &SlackResponse{"Hello World"}
+	if m.Text == "" {
+		return errors.New("Don't know who to say hello to. Please specify a name"), nil
+	}
+	return nil, &SlackResponse{"Hello " + m.Text}
+}
+
+type PingAction struct{}
+
+func (a PingAction) HandleAction(m *SlackMessage) (error, *SlackResponse) {
+	return nil, &SlackResponse{"Pong"}
+}
+
+func init() {
+	RegisterHandler("ping", PingAction{})
 }
